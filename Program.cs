8355ff3a77cd7b232c8c08e32e18571ade782c04
Rodrigo <rodrigocsilva291@gmail.com>
2025-10-24 21:23:48 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+
+bool t, p, r, w;
+
+Console.WriteLine("--- Simulador de Luz de Advertência ---\n");
+Console.WriteLine("[A]tivado, [outro] Desativado\n");
+
+Console.Write("Sensor de temperatura...: ");
+string sensorT = Console.ReadLine()!.ToUpper();
+t = (sensorT == "A");
+
+Console.Write("Sensor de pressão.......: ");
+string sensorP = Console.ReadLine()!.ToUpper();
+p = (sensorP == "A");
+
+Console.Write("Sensor de rotação.......: ");
+string sensorR = Console.ReadLine()!.ToUpper();
+r = (sensorR == "A");
+
+w = t && (p || !r);
+
+string resultado;
+
+if (w)
+{
+    resultado = "ATIVADA";
+}
+else
+{
+    resultado = "DESATIVADA";
+}
+
+// resultado = w ? "ATIVADA" : "DESATIVADA";
+
+Console.WriteLine($"\nA luz de advertência estará {resultado}.");
